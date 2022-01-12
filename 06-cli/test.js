52b76c2b9b1c5eb07fe00b2856383a1b29f0e96c
Renan <rna.domingos@hotmail.com>
2022-01-12 07:43:17 -0300

    Modulo 4 - Manipulando arquivos - READ

---
+++ 06-cli/test.js
@@ -0,0 +1,29 @@
+const { 
+    deepEqual,
+    ok
+} = require('assert')
+
+const database = require('./database')
+
+const DEFAULT_ITEM_CADASTRAR = { 
+    nome: 'Flash',
+    poder: 'Speed',
+    id: 1
+}
+
+describe('Suite de manipulação de Herois', () => {
+    
+    it('deve pesquisar um heroi usando arquivos', async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR
+        
+        // abaixo é utilizado a tecnica de destructor "[resultado]" desta forma ele retorno o primeiro item do array no caso o item "resultado[0]"
+        // caso queira usar itens da segunda posição utilize [resultado, posicao2, posicao3]
+        const [resultado] = await database.listar(expected.id)
+        deepEqual(resultado, expected)
+    })
+    // it('deve cadastrar um heroi, usando arquivos', async () =>{
+    //     const expected = DEFAULT_ITEM_CADASTRAR
+    //     //
+    //     ok(null, expected)
+    // })
+})
