9c744f03308cc28d29fb0a59eaa6049494b7efe2
Renan <rna.domingos@hotmail.com>
2022-02-08 19:06:16 -0300

    Modulo 7 - Listando Herois - READ

--- 09-multi-db-postgres-mongo/src/db/strategies/base/contextStrategy.js
+++ 09-multi-db-postgres-mongo/src/db/strategies/base/contextStrategy.js
@@ -9,8 +9,8 @@ class ContextStrategy {
         return this._database.create(item)
     }
 
-    read(item) {
-        return this._database.read(item)
+    read(item, skip, limit) {
+        return this._database.read(item, skip, limit)
     }
 
     update(id, item){
--- 09-multi-db-postgres-mongo/src/tests/mongodbStrategy.test.js
+++ 09-multi-db-postgres-mongo/src/tests/mongodbStrategy.test.js
@@ -7,11 +7,15 @@ const MOCK_HEROI_CADASTRAR = {
     nome: 'Mulher Maravilha',
     poder: 'Laço'
 }
-
+const MOCK_HEROI_DEFAULT = {
+    nome: `Homem Arannha=${Date.now()}`,
+    poder: 'Super Teia'
+}
 const context = new Context(new MongoDB())
 describe('MongoDB Suite de Testes', function () {
     this.beforeAll(async () => {
         await context.connect()
+        await context.create(MOCK_HEROI_DEFAULT)
     } )
     it('verificar conexao', async () => {
         const result = await context.isConnected()
@@ -24,5 +28,13 @@ describe('MongoDB Suite de Testes', function () {
     it('cadastrar', async () => {
         const { nome, poder } = await context.create(MOCK_HEROI_CADASTRAR)
         assert.deepEqual({ nome, poder }, MOCK_HEROI_CADASTRAR)
+    })
+
+    it('listar', async () => {
+        const [{ nome, poder }] = await context.read({ nome: MOCK_HEROI_DEFAULT.nome })
+        const result = {
+            nome, poder
+        }
+        assert.deepEqual(result, MOCK_HEROI_DEFAULT)
     })
 })
