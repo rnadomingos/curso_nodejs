3d60f35a785f996957c4196fc429762edaec2033
Renan <rna.domingos@hotmail.com>
2022-03-07 23:53:23 -0300

    Modulo 12 - Expondo Cobertura de Codigo com Istambul

---
+++ 25-heroku-istambul/git_renan-cursonodebr-renan/api.js
@@ -0,0 +1,117 @@
+// npm install hapi
+// npm install Vision Inert hapi-swagger
+
+// npm i hapi-jwt2
+
+// npm i bcrypt
+
+// npm i dotenv
+
+const { config } = require('dotenv')
+const { join } = require('path')
+const { ok } = require('assert')
+
+const env = process.env.NODE_ENV || "dev"
+
+ok(env === "prod" || env === "dev", "A env é invalida, utilize dev ou prod")
+
+const configPath = join(__dirname, './config', `.env.${env}`)
+
+config({
+    path: configPath
+})
+
+const Hapi = require('@hapi/hapi')
+const MongoDb = require('./src/db/strategies/mongodb/mongodb')
+const Context = require('./src/db/strategies/base/contextStrategy')
+const HeroiSchema = require('./src/db/strategies/mongodb/schemas/heroisSchemas')
+const HeroRoutes = require('./src/routes/heroRoutes')
+const AuthRoutes = require('./src/routes/authRoutes')
+
+const Postgres = require('./src/db/strategies/postgres/postgres')
+const UsuarioSchema = require('./src/db/strategies/postgres/schemas/usuarioSchema')
+
+const HapiSwagger = require('hapi-swagger')
+const Vision = require('@hapi/vision')
+const Inert = require('@hapi/inert')
+const HapiJwt = require('hapi-auth-jwt2')
+const UtilRoutes = require('./src/routes/utilRoutes')
+
+const JWT_SECRET = process.env.JWT_KEY
+
+const app = new Hapi.Server({
+    port: process.env.PORT
+})
+
+function mapRoutes(instance, methods) {
+    return methods.map(method => instance[method]())
+
+}
+
+async function main() {
+
+    const connection = MongoDb.connect()
+    const context = new Context(new MongoDb(connection, HeroiSchema))
+
+    const connectionPostgres = await Postgres.connect()
+    const model = await Postgres.defineModel(connectionPostgres, UsuarioSchema)
+    const contextPostgres = new Context(new Postgres(connectionPostgres, model))
+
+    const swaggerOptions = {
+        info: {
+            title: 'API Herois - #CursoNodeBR',
+            version: 'v1.0'
+        }
+    }
+     
+    await app.register([
+        HapiJwt,
+        Vision,
+        Inert,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ])
+
+    app.auth.strategy('jwt', 'jwt', {
+        key: JWT_SECRET,
+        // options: {
+        //     expiresIn: 20
+        // }
+        validate: async (dados, request) => {
+            // verifica no banco se usuario continua ativo
+            // verifica no banco se usuario continua pagando
+
+            const [result] = await contextPostgres.read({
+                username: dados.username.toLowerCase()
+            })
+            if (!result) {
+                return { 
+                    isValid: false
+                }
+                
+            }
+            return {
+                isValid: true // caso nao valido, false
+            }
+        }
+    })
+
+    app.auth.default('jwt')
+
+
+    app.route([
+        ...mapRoutes(new HeroRoutes(context), HeroRoutes.methods()),
+        ...mapRoutes(new AuthRoutes(JWT_SECRET, contextPostgres), AuthRoutes.methods()),
+        ...mapRoutes(new UtilRoutes(),UtilRoutes.methods())
+    ])
+    
+
+    await app.start()
+    console.log('Servidor rodando na porta', app.info.port);
+
+    return app
+}
+
+module.exports = main()
